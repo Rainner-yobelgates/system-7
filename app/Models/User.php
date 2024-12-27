41b193a7b5c0099eab79f314c0ec55b70c0c3672
Rainner Yobelgates <ryfranceisten@gmail.com>
2024-12-27 09:14:27 +0700

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +14,11 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return !(is_null($this->user_role_id));
+    }
+    
     /**
      * The attributes that are mass assignable.
      *
