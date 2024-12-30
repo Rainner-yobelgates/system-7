689c4fea0238e75335ff4ea6e77cb231b02a6a5c
Rainner Yobelgates <ryfranceisten@gmail.com>
2024-12-30 20:00:45 +0700

    install liveware

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,7 +33,6 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'serve' => true,
             'throw' => false,
         ],
 
