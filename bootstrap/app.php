755b2b679480b40dbe7e393490de91a676580254
Rainner Yobelgates <ryfranceisten@gmail.com>
2024-12-31 13:10:50 +0700

    update proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
+        // $middleware->web(append: [
+        //     HandleInertiaRequests::class,
+        // ]);
         $middleware->trustProxies(at: '*');
         $middleware->trustProxies(
             headers: Request::HEADER_X_FORWARDED_FOR |
